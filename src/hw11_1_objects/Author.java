1e6adbdacbd8530c268412023360d9f9bac356f0
SvetlanaShin <susperson@gmail.com>
2022-11-04 02:13:08 +0600

    ДЗ 12. Методы объектов.

--- src/hw11_1_objects/Author.java
+++ src/hw11_1_objects/Author.java
@@ -1,5 +1,7 @@
 package hw11_1_objects;
 
+import java.util.Objects;
+
 public class Author {
     private final String firstName;
     private final String lastName;
@@ -17,7 +19,21 @@ public class Author {
         return this.lastName;
     }
 
-    public void printAuthor() {
-        System.out.println(this.getFirstName() + " " + this.getLastName());
+    @Override
+    public String toString() {
+        return "Автор: " + firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object otherAuthor) {
+        if (this == otherAuthor) return true;
+        if (otherAuthor == null || getClass() != otherAuthor.getClass()) return false;
+        Author author = (Author) otherAuthor;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
